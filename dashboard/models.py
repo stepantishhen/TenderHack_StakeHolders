beb4bb8e2110de4b100463600d330ee7ba836a58
Stepan Tischenko <stepantishhen@mail.ru>
2023-10-21 14:46:29 +0300

    Database structure

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 class Error(models.Model):
     error = models.CharField(max_length=255)
+    # status, priority
     other_details = models.TextField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -18,11 +19,7 @@ class Notification(models.Model):
 class SystemLog(models.Model):
     date_time = models.DateTimeField()
     log = models.TextField()
-    category = models.CharField(max_length=255)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, null=True)
 
 
 class Ticket(models.Model):
@@ -30,6 +27,4 @@ class Ticket(models.Model):
     text = models.TextField()
     performer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_tickets')
     initiator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tickets')
-
-    def __str__(self):
-        return self.subject
+    status = models.CharField(max_length=255)
